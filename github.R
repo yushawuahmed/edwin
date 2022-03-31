dfa7947f55910259dfffab73e9bf049c7cc85969
Edwinappiah18 <edwinappiah@yahoo.com>
2022-03-31 12:32:04 -0400

    git

--- github.R
+++ github.R
@@ -3,3 +3,8 @@ library(usethis)
 create_github_token()
 edit_r_environ()
 use_github(protocol = 'https', auth_token = Sys.getenv("GITHUB_PAT"))
+
+usethis::use_git_config(user.name = "Edwinappiah18", user.email = "edwinappiah@yahoo.com")
+usethis::create_github_token() 
+credentials::set_github_pat("/home/edwin/edwin.git/")
+usethis::edit_r_environ()
